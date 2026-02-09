0dd6dd17143562256bc8be814a35f2997a5e3fd0
Marcus Branney <mbranney12@icloud.com>
2026-02-09 15:05:17 +0000

    initial commit

---
+++ gMyBinds.sh
@@ -0,0 +1,8 @@
+# remap capslock to escape
+gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
+
+# super + k to quit
+
+# super + enter for terminal
+
+# super + f for firefox
---
+++ revertcaps2esc.sh
@@ -0,0 +1,1 @@
+gsettings reset org.gnome.desktop.input-sources xkb-options 
